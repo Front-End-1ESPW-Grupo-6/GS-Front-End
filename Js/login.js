82a720a7cd5d55a0cb234f009373a54b151a8e8d
Guimeb <rm551981@fiap.com.br>
2023-06-01 01:17:07 -0300

    atualizado e limpado sistema de cadastro

--- Js/login.js
+++ Js/login.js
@@ -64,8 +64,9 @@ signupButton.addEventListener("click", (evt) => {
         name: nameInput,
         nick: nicknameInput,
         accountType: "user",
-        emailUser: emailInput,
-        senhaUser: senhaInput
+        email: emailInput,
+        number: numberInput,
+        password: senhaInput
     }
     console.log(newUser)
     //Store new user
@@ -80,14 +81,112 @@ signupButton.addEventListener("click", (evt) => {
     let nameInputClear = document.querySelector('#name-Signup-Input');
     let nicknameInputClear = document.querySelector('#nick-Signup-Input');
     let emailInputClear = document.querySelector('#email-Signup-Input');
+    let numberInputClear = document.querySelector('#number-Signup-Input');
     let passwordInputClear = document.querySelector('#pass-Signup-Input');
     let passConfirmInputClear = document.querySelector('#passConfirm-Signup-Input');
     nameInputClear.value = ""
     nicknameInputClear.value = ""
     emailInputClear.value = ""
+    numberInputClear.value = ""
     passwordInputClear.value = ""
     passConfirmInputClear.value = ""
 })
+
+//ONG signup system
+signupOngButton.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    //ONG signup var
+    let nameOngInput = document.querySelector('#name-OSignup-Input').value;
+    let agentOngInput = document.querySelector('#agent-OSignup-Input').value;
+    let emailOngInput = document.querySelector('#email-OSignup-Input').value;
+    let numberOngInput = document.querySelector('#number-Signup-Input').value;
+    let cnpjOngInput = document.querySelector('#cnpj-OSignup-Input').value;
+    let addressOngInput = document.querySelector('#address-OSignup-Input').value;
+    let passwordOngInput = document.querySelector('#pass-OSignup-Input').value;
+    let passConfirmOngInput = document.querySelector('#passConfirm-OSignup-Input').value;
+    //Signup Confirm
+    if (nameOngInput == "" || agentOngInput == "" || emailOngInput == "" || numberOngInput == "" || cnpjOngInput == "" || addressOngInput == "" || passwordOngInput == "" || passConfirmOngInput == "") {
+        console.log('Todos os campos devem ser preenchidos');
+        signupOngMsgAlert.innerHTML = 'Todos os campos devem ser preenchidos';
+        signupOngMsgAlert.classList.add('');
+        return;
+    }
+    //Email
+    if (emailOngInput.length < 5) {
+        console.log('Email deve conter mais de 5 letras');
+        signupOngMsgAlert.innerHTML = 'Email deve conter mais de 5 caracteres';
+        signupOngMsgAlert.classList.add('');
+        return;
+    } else if (emailInput.indexOf('@') === -1) {
+        console.log('Email invalido, deve conter "@"');
+        signupOngMsgAlert.innerHTML = 'Email invalido, deve conter "@"';
+        signupOngMsgAlert.classList.add('');
+        return;
+    }
+    //Number
+    if (numberOngInput = 0){
+        console.log('0')
+    }
+    //Cnpj
+    if (cnpjOngInput = 0){
+        console.log('0')
+    }
+    //Password
+    if (passwordOngInput.length < 5) {
+        console.log('Senha deve conter no minimo 5 digitos');
+        signupOngMsgAlert.innerHTML = 'Senha deve conter no minimo 5 digitos'
+        signupOngMsgAlert.classList.add('');
+        return;
+    }
+    //Password Confirm
+    if (passConfirmOngInput != passwordOngInput) {
+        console.log('Senhas digitadas não coincidem');
+        signupOngMsgAlert.innerHTML = 'Senhas digitadas não coincidem';
+        signupOngMsgAlert.classList.add('');
+        return;
+    }
+
+    const newUser = {
+        name: nameOngInput,
+        ongAgent: agentOngInput,
+        emailUser: emailOngInput,
+        number: numberOngInput,
+        cnpj: cnpjOngInput,
+        password: passwordOngInput,
+        accountType: "ONG"
+    }
+    console.log(newUser)
+    //Store new user
+    let UsersListBack = JSON.parse(localStorage.getItem("usersList"));
+    UsersListBack.push(newUser);
+    localStorage.setItem("usersList", JSON.stringify(UsersListBack));
+
+    //Site feedback
+    console.log('Cadastro efetuado com sucesso!');
+    signupMsgAlert.innerHTML = 'Cadastro efetuado com sucesso!';
+    signupMsgAlert.classList.add('');
+
+    //Text box clear
+    let nameOngInputClear = document.querySelector('#name-OSignup-Input');
+    let agentOngInputClear = document.querySelector('#agent-OSignup-Input');
+    let emailOngInputClear = document.querySelector('#email-OSignup-Input');
+    let numberOngInputClear = document.querySelector('#number-Signup-Input');
+    let cnpjOngInputClear = document.querySelector('#cnpj-OSignup-Input');
+    let addressOngInputClear = document.querySelector('#address-OSignup-Input');
+    let passwordOngInputClear = document.querySelector('#pass-OSignup-Input');
+    let passConfirmOngInputClear = document.querySelector('#passConfirm-OSignup-Input');
+    nameOngInputClear.value = ""
+    agentOngInputClear.value = ""
+    emailOngInputClear.value = ""
+    numberOngInputClear.value = ""
+    cnpjOngInputClear.value = ""
+    addressOngInputClear.value = ""
+    passwordOngInputClear.value = ""
+    passConfirmOngInputClear.value = ""
+})
+
+
+
 //Evento de olho
 let eyeLogin = document.querySelector('#openLEye')
 eyeLogin.addEventListener('click', () => {
